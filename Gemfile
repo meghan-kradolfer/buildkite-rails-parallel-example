800ca8e73d9d6dda061c35406aa0843911d64293
niceking <rose@buildkite.com>
2023-03-20 15:07:32 +1300

    add test collector ruby

--- Gemfile
+++ Gemfile
@@ -69,6 +69,7 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem "buildkite-test_collector"
 end
 
 group :test, :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
-
 require 'knapsack'
+require "buildkite/test_collector"
 
 Knapsack::Adapters::RSpecAdapter.bind
+
+Buildkite::TestCollector.configure(
+  hook: :rspec,
+  token: "xq2CgNsjxWoCFTuWKCuJ8YZg",
+  url: "http://analytics-api.buildkite.localhost/v1/uploads"
+)
+
