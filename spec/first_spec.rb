97c45db62a185b8419baabb7b083c16d7039cb1c
Meghan <meghan.kradolfer@gmail.com>
2023-04-11 13:53:40 +1200

    Still failing

--- spec/first_spec.rb
+++ spec/first_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe "First spec" do
   it "runs in parallel" do
     expect(1).to eql(1)
-    assert(false)
+  end
+  specify "this test fails" do
+    raise "this is my failure message"
   end
 end
