b99e14615b853d30296216ec3149fc9e566cea94
Meghan <meghan.kradolfer@gmail.com>
2024-08-29 09:24:05 +1200

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ Knapsack::Adapters::RSpecAdapter.bind
 
 Buildkite::TestCollector.configure(
   hook: :rspec,
-  token: "oeHxhhEe42DooXpDvsegJJKo",
+  token: "xx-local-analytics-key",
   url: "http://analytics-api.buildkite.localhost/v1/uploads"
 )
 
